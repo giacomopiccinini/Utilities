2b730110152285dc20288cd608e9ca5d3be9cb1a
Giacomo <ubuntu@ip-172-31-32-63.eu-west-1.compute.internal>
2022-07-12 08:54:13 +0000

    Fix

--- .ipynb_checkpoints/make_global-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-script=$1
-
-chmod +x "$script".sh
-
-sudo cp "$script".sh /bin/"$script"
--- .ipynb_checkpoints/requirements-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Create requirements.txt for the code in the directory where it is launched from 
-pipreqs .
