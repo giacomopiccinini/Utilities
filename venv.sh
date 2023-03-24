c08064406ef19998fd9bca43a19c4b52f608a356
giacomopiccinini <piccinini.giacomo@gmail.com>
2023-03-24 09:46:01 +0100

    [FEAT] Pre-commits for black, and improved .gitignore

--- venv.sh
+++ venv.sh
@@ -28,5 +28,43 @@ environment=$(basename "$PWD")
 # Install the current environment so that it can be used as a Kernel
 python -m ipykernel install --user --name="$environment"
 
+# Basic libraries we will use
+echo "black" >> requirements.txt
+echo "pipreqs" >> requirements.txt
+echo "numpy" >> requirements.txt
+echo "pre-commit" >> requirements.txt
+echo "tqdm" >> requirements.txt
+echo "python-decouple" >> requirements.txt
+
 # Install requirements
 pip install -r requirements.txt
+
+# Create precommits
+echo "repos:" >> .pre-commit-config.yaml
+echo "  - repo: https://github.com/psf/black" >> .pre-commit-config.yaml
+echo "    rev: stable" >> .pre-commit-config.yaml
+echo "    hooks:" >> .pre-commit-config.yaml
+echo "      - id: black" >> .pre-commit-config.yaml
+echo "        language_version: "$pversion >> .pre-commit-config.yaml
+
+if 	git rev-parse --is-inside-work-tree; then
+    # Install precommit
+    pre-commit install
+    # Add few things to gitignore
+    echo "*.ipynb" >> .gitignore
+    echo "*.jpg" >> .gitignore
+    echo "*.png" >> .gitignore
+    echo "*.mp4" >> .gitignore
+    echo "*.hdf5" >> .gitignore
+    echo "*.pt" >> .gitignore
+    echo "*.pth" >> .gitignore
+    echo ".ipynb_checkpoints" >> .gitignore
+    echo "__pycache__" >> .gitignore
+    echo "venv" >> .gitignore
+    echo ".env" >> .gitignore
+    echo ".pytest_cache" >> .gitignore
+    echo "*.mp3" >> .gitignore
+    echo "*.wav" >> .gitignore
+else
+    echo "Not a git repository"
+fi;
